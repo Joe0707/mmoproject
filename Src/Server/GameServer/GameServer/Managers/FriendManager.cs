02cc39d16aa22f8d6140308c3e93dfc392328e07
jiaochunyu <2206357676@qq.com>
2020-11-30 08:31:07 +0800

    xiugai

---
+++ Src/Server/GameServer/GameServer/Managers/FriendManager.cs
@@ -0,0 +1,110 @@
+﻿using GameServer.Entities;
+using GameServer.Services;
+using SkillBridge.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.Managers
+{
+    class FriendManager
+    {
+        Character Owner;
+        List<NFriendInfo> friends = new List<NFriendInfo>();
+        bool friendChanged = false;
+        public FriendManager(Character owner)
+        {
+            this.Owner = owner;
+            this.InitFriends();
+        }
+
+        public void GetFriendInfos(List<NFriendInfo> list)
+        {
+            foreach(var f in this.friends)
+            {
+                list.Add(f);
+            }
+        }
+
+        public void InitFriends()
+        {
+            this.friends.Clear();
+            foreach(var friend in this.Owner.Data.Friends)
+            {
+                this.friends.Add(GetFriendInfo(friend));
+            }
+        }
+
+        public void AddFriend(Character friend)
+        {
+            TCharacterFriend tf = new TCharacterFriend()
+            {
+                FriendID = friend.Id,
+                FriendName = friend.Name,
+                Class = friend.Data.Class,
+                Level = friend.Data.Level
+            };
+            this.Owner.Data.Friends.Add(tf);
+            friendChanged = true;
+        }
+
+        public bool RemoveFriendByFriendId(int friendid)
+        {
+            var removeItem = this.Owner.Data.Friends.FirstOrDefault(v => v.FriendID == friendid);
+            if(removeItem!=null)
+            {
+                DBService.Instance.Entities.CharacterFriends.Remove(removeItem);
+            }
+            friendChanged = true;
+            return true;
+        }
+
+        public bool RemoveFriendByID(int id)
+        {
+            var removeItem = this.Owner.Data.Friends.FirstOrDefault(v => v.Id == id);
+            if(removeItem!=null)
+            {
+                DBService.Instance.Entities.CharacterFriends.Remove(removeItem);
+            }
+            friendChanged = true;
+            return true;
+        }
+
+        public NFriendInfo GetFriendInfo(TCharacterFriend friend)
+        {
+            NFriendInfo friendInfo = new NFriendInfo();
+            var character = CharacterManager.Instance.GetCharacter(friend.FriendID);
+            friendInfo.friendInfo = new NCharacterInfo();
+            friendInfo.Id = friend.Id;
+            return friendInfo;
+        }
+
+        public void UpdateFriendInfo(NCharacterInfo friendInfo,int status)
+        {
+            foreach(var f in this.friends)
+            {
+                if(f.friendInfo.Id==friendInfo.Id)
+                {
+                    f.Status = status;
+                    break;
+                }
+            }
+        }
+
+        public void PostProcess(NetMessageResponse message)
+        {
+            if(friendChanged)
+            {
+                this.InitFriends();
+            }
+            if (message.friendList ==null)
+            {
+                message.friendList = new FriendListResponse();
+                message.friendList.Friends.AddRange(this.friends);
+            }
+            friendChanged = false;
+        }
+    }
+}
---
+++ Src/Server/GameServer/GameServer/Services/DBService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Common;
+
+namespace GameServer.Services
+{
+    class DBService : Singleton<DBService>
+    {
+        ExtremeWorldEntities entities;
+
+
+
+        //public ExtremeWorldEntities Entities
+        //{
+        //    get { return this.entities; }
+        //}
+
+        public void Init()
+        {
+            //entities = new ExtremeWorldEntities();
+
+           
+        }
+
+        public void Save(bool async = false)
+        {
+            //也可以定时存储
+            //if (async)
+            //    entities.SaveChangesAsync();
+            //else
+            //    entities.SaveChanges();
+        }
+    }
+}
